b2e4b703a039c7f41d50231cb426f9904278cffb
KiwiGin <bayeskiwigen@gmail.com>
2024-02-24 12:48:34 -0500

    j

--- js/actas_ubigeo.js
+++ js/actas_ubigeo.js
@@ -1,17 +1,41 @@
-const getDepartamentos = async (ambito) => {
+ubigeo = ''
+const getDepartamentos = async (ubigep) => {
     const data = await fetch(`https://oaemdl.es/onpe_sweb_php/actas/ubigeo/${ambito}`)
     if (data.status == 200) {
         const departamentos = await data.json()
         let html = `
-            <select name="cdgoDep" id="cdgoDep" class="form-control" onchange="getProvinciasDepa_acta('', '', this.value);">
+            <select name="cdgoDep" id="cdgoDep" class="form-control" onchange="getProvincias('', '', this.value);"disabled>
                             <option selected="selected" value="">--SELECCIONE--</option>
             `
         departamentos.forEach(departamento => {
             html += `
-                <option value="${departamento.id}"${departamento.detalle}</option>
+                <option value="${departamento.Detalle}"${departamento.Detalle}</option>
                 `
         });
         html += '</select>'
-        document.getElementById('departamentos').innerHTML=html
+        document.getElementById('departamentos').innerHTML = html
+        document.getElementById('provincias').innerHTML = html`
+        <select name="cdgoProv" id="cdgoProv" class="form-control" onchange="getDistrito(this.value);"disabled>
+        <option selected="selected" value="">--SELECCIONE--</option>
+        
+        `
+    }
+}
+
+const getProvincias = async (departamento) => {
+    const data = await fetch(`https://oaemdl.es/onpe_sweb_php/actas/ubigeo/${ambito}/${departamento}`)
+    if (data.status == 200) {
+        const departamentos = await data.json()
+        let html = `
+        <select name="cdgoProv" id="cdgoProv" class="form-control" onchange="getDistrito(this.value);"disabled;
+                            <option selected="selected" value="">--SELECCIONE--</option>
+            `
+        provincias.forEach(provincia => {
+            html += `
+                <option value="${provincia.Detalle}"${provincia.Detalle}</option>
+                `
+        });
+        html += '</select>'
+        document.getElementById('provincias').innerHTML = html
     }
 }
