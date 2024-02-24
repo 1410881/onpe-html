1c98aaea671681841fa4d08be6e02ea9d81d07a0
KiwiGin <bayeskiwigen@gmail.com>
2024-02-24 12:19:10 -0500

    js

--- js/actas_ubigeo.js
+++ js/actas_ubigeo.js
@@ -1,4 +1,17 @@
-const getDepartamentos = async()=>{
-
-    
+const getDepartamentos = async (ambito) => {
+    const data = await fetch(`https://oaemdl.es/onpe_sweb_php/actas/ubigeo/${ambito}`)
+    if (data.status == 200) {
+        const departamentos = await data.json()
+        let html = `
+            <select name="cdgoDep" id="cdgoDep" class="form-control" onchange="getProvinciasDepa_acta('', '', this.value);">
+                            <option selected="selected" value="">--SELECCIONE--</option>
+            `
+        departamentos.forEach(departamento => {
+            html += `
+                <option value="${departamento.id}"${departamento.detalle}</option>
+                `
+        });
+        html += '</select>'
+        document.getElementById('departamentos').innerHTML=html
+    }
 }
